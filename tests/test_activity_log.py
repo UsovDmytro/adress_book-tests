dfc9d19fcbd92bb782efd9afe1da745432b95f81
Usov Dmytro <dioks.2013@gmail.com>
2024-01-09 03:39:05 +0200

    activity log create test

--- tests/test_activity_log.py
+++ tests/test_activity_log.py
@@ -20,32 +20,3 @@ def test_activity_log_create(contact_factory):
        assert act_log.contact == contact1
     except:
         assert False
-
-
-# @pytest.fixture
-# def contact():
-#     return Contact.objects.create(
-#         first_name="John",
-#         last_name="Doe",
-#         country="USA",
-#         city="NY",
-#         street="Main street",
-#         url="https://example.com",
-#         phone="+1234567890",
-#         image=None  # можна додати зображення, якщо потрібно
-#     )
-#
-#
-# # Тест на перевірку серіалізації
-# @pytest.mark.django_db
-# def test_contact_serializer(contact):
-#     serialized = ContactSerializer(contact)
-#
-#     assert serialized.data["first_name"] == "John"
-#     assert serialized.data["last_name"] == "Doe"
-#     assert serialized.data["country"] == "USA"
-#     assert serialized.data["city"] == "NY"
-#     assert serialized.data["street"] == "Main street"
-#     assert serialized.data["url"] == "https://example.com"
-#     assert serialized.data["phone"] == "+1234567890"
-#     assert serialized.data["image"] is None  # якщо у вас є зображення, перевірте його URL тут
