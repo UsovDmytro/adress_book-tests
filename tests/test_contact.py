e29d395e719a23e9ccbcbc77cb01fb0bfcd82614
Usov Dmytro <dioks.2013@gmail.com>
2024-01-09 04:49:37 +0200

    contactSerializer create test

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -19,3 +19,33 @@ def test_contact_mocked_create(contact_factory, mocker):
     contact2 = contact_factory(first_name="Jane", city="LA")
     assert Contact.objects.all().count() == 0
 
+#!!!!ЗАДАНИЕ 5 - в исходной базе уже имеется решение, которое и скопировал сюда:)
+# возможно есть смысл в дальнейшем удалить этот код с исходной базы
+# или изменить задание подразумевающее как то дополнить тест на основе имеющегося
+@pytest.fixture
+def contact():
+    return Contact.objects.create(
+        first_name="John",
+        last_name="Doe",
+        country="USA",
+        city="NY",
+        street="Main street",
+        url="https://example.com",
+        phone="+1234567890",
+        image=None  # можна додати зображення, якщо потрібно
+    )
+
+
+# Тест на перевірку серіалізації
+@pytest.mark.django_db
+def test_contact_serializer(contact):
+    serialized = ContactSerializer(contact)
+
+    assert serialized.data["first_name"] == "John"
+    assert serialized.data["last_name"] == "Doe"
+    assert serialized.data["country"] == "USA"
+    assert serialized.data["city"] == "NY"
+    assert serialized.data["street"] == "Main street"
+    assert serialized.data["url"] == "https://example.com"
+    assert serialized.data["phone"] == "+1234567890"
+    assert serialized.data["image"] is None  # якщо у вас є зображення, перевірте його URL тут
