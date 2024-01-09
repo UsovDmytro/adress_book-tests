1cf513abf6ff77669406ae6d1ef9baccccc7f69d
Usov Dmytro <dioks.2013@gmail.com>
2024-01-09 04:35:14 +0200

    activity log mock create test

--- tests/test_activity_log.py
+++ tests/test_activity_log.py
@@ -32,3 +32,19 @@ def test_activity_log_edit(contact_factory):
        assert act_log.contact == contact1
     except:
         assert False
+
+
+@pytest.mark.django_db
+def test_activity_log_mocked_create(contact_factory, mocker):
+    mock_create = mocker.patch.object(ContactActivityLog, 'save', autospec=True)
+    contact1 = contact_factory(first_name="John", city="NY")
+    contact2 = contact_factory(first_name="Jane", city="LA")
+    contact1.first_name = 'Doe'
+    contact1.save()
+    assert mock_create.call_count == 3
+    assert mock_create.call_args[0][0].activity_type == "EDITED"
+    try:
+       contact_db = Contact.objects.get(pk=contact1.pk)
+       assert contact_db.first_name == 'Doe'
+    except:
+        assert False
